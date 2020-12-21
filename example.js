c13a255f1ae89f5fafe898a60540d19b7b3faf0c
Karang <arthur.hennequin@free.fr>
2020-12-22 00:08:46 +0100

    Add writing

--- example.js
+++ example.js
@@ -3,7 +3,11 @@ const { Schematic } = require('prismarine-schematic')
 async function main () {
   const schematic = await Schematic.read('test/schematics/smallhouse1.schem')
 
-  console.log(schematic)
+  schematic.write('test.schem')
+
+  const schematic2 = await Schematic.read('test.schem')
+
+  console.log(schematic2)
 }
 
 main()
