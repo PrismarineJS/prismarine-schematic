f404ed89518d1fac0f843d8a36766d6772b874ce
IceTank <61137113+IceTank@users.noreply.github.com>
2021-07-10 15:17:32 +0200

    Fixed test (#33)

--- test/setblock.test.js
+++ test/setblock.test.js
@@ -10,7 +10,7 @@ const buffer = fs.readFileSync(path.join(__dirname, 'schematics', 'viking-house1
 describe('setblock test', () => {
   test('viking house', async () => {
     const schem = await Schematic.read(buffer, '1.16.4')
-    const commands = await schem.makeWithCommands(new Vec3(0, 0, 0))
-    expect(commands[901]).toStrictEqual('/setblock -7 0 13 oak_trapdoor [waterlogged="true",powered="true",open="true",half="bottom",facing="west"]')
+    const commands = await schem.makeWithCommands(new Vec3(0, 0, 0), 'pc')
+    expect(commands[901]).toStrictEqual('/setblock -7 0 13 oak_trapdoor[waterlogged="true",powered="true",open="true",half="bottom",facing="west"]')
   })
 })
