efca116534e216057cc1ff52179a638b9e1dae56
IceTank <niklaslehle@gmail.com>
2021-07-25 20:04:42 +0200

    More tests

--- test/arrayfuncs.test.js
+++ test/arrayfuncs.test.js
@@ -37,13 +37,18 @@ describe('editing test', () => {
     schem.setBlock(new Vec3(1, 3, 1), new Block(mcData.blocksByName.stone.id, 1, 0))
 
     expect(schem.getBlock(new Vec3(1, 1, 1)).type).toStrictEqual(mcData.blocksByName.chest.id)
+    expect(schem.getBlock(new Vec3(1, 2, 1)).type).toStrictEqual(mcData.blocksByName.dirt.id)
     expect(schem.getBlock(new Vec3(1, 3, 1)).type).toStrictEqual(mcData.blocksByName.stone.id)
 
-    // Now remove them
+    // Now remove some
     schem.setBlock(new Vec3(1, 1, 1), null)
     schem.setBlock(new Vec3(1, 3, 1), null)
 
     expect(schem.getBlock(new Vec3(1, 1, 1)).type).toStrictEqual(mcData.blocksByName.air.id)
+    expect(schem.getBlock(new Vec3(1, 2, 1)).type).toStrictEqual(mcData.blocksByName.dirt.id)
     expect(schem.getBlock(new Vec3(1, 3, 1)).type).toStrictEqual(mcData.blocksByName.air.id)
+
+    expect(schem.palette.filter(b => !!b).length).toStrictEqual(1)
+    expect(schem.blocks.filter(b => !!b).length).toStrictEqual(1)
   })
 })
